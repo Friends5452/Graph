8d6afd2b1b7d867c1d5aedf6f0324306b4687955
AnanTripathi <aman.tripathi046@gmail.com>
2020-02-26 22:15:27 +0530

    Commit after adding the fragments

---
+++ app/src/main/java/com/example/multiplegraphs/Fragment2.java
@@ -0,0 +1,31 @@
+package com.example.multiplegraphs;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Fragment2 extends Fragment {
+
+
+    public Fragment2() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment2, container, false);
+        return view;
+    }
+
+}
